777a000311f726d75e440184169a385714c7d497
andre <daiandre11301@gmail.com>
2019-03-16 16:39:52 -0400

    springboot+springsecurite

---
+++ src/main/java/com/dyk/springsecurite/SpringsecuriteApplication.java
@@ -0,0 +1,13 @@
+package com.dyk.springsecurite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecuriteApplication.class, args);
+    }
+
+}
